ddfc977378c61e37b9edb0c1ba3b0cd6d591f6b0
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2020-06-13 18:59:11 +0200

    Add missing dependencies

--- data_node/types.go
+++ data_node/types.go
@@ -1,5 +1,7 @@
 package datanode
 
+import "sync"
+
 //DataNode Represents a data storage node in the system
 type DataNode struct {
 	IP           string       //IP of the data node host
@@ -12,3 +14,28 @@ type NameNodeData struct {
 	IP   string //IP of the name node host
 	Port string //Port on which the data node communicates with the name node
 }
+
+// UploadManager represents storage to keep files info
+// and keeps track of what files are currently in data node
+type UploadManager struct {
+	fileBase map[string]FileInfo // Holds information about files available in data node
+	mux      sync.RWMutex        // For safe concurrent access to filebase
+}
+
+// FileInfo represents file information on disk
+type FileInfo struct {
+	Name        string   // Name of file
+	Type        fileType // Indicates type of file (video, model .... etc)
+	Path        string   // Path to file (excluding file name), with trailing slash
+	Offset      int64    // Offset of bytes to start writing data at
+	Size        int64    // Total size of file in bytes
+	isCompleted bool     //Indicates if file completed uploading
+}
+
+type fileType string
+
+const (
+	videoFile  fileType = "VideoFile"
+	modelFile           = "ModelFile"
+	configFile          = "ConfigFile"
+)
--- data_node/utils.go
+++ data_node/utils.go
@@ -1,3 +1,22 @@
 package datanode
 
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+)
+
 //Here goes any utils that are specific to data node
+
+// createFileDirectory creates a directory with given permission
+func createFileDirectory(dirPath string, perm os.FileMode) error {
+	err := os.MkdirAll(dirPath, perm)
+	return err
+}
+
+// generateRandomString generates random string with length n
+func generateRandomString(n int) string {
+	b := make([]byte, n)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
